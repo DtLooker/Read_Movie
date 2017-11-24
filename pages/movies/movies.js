a114f80517ae71496bcc79475320e6d5048b652c
DtLooker <dingt90@163.com>
2017-11-24 10:21:24 +0800

    完成电影资讯页面部分

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -12,12 +12,19 @@ Page({
         var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters" + "?start=0&count=3";
         var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon" + "?start=0&count=3";
         var top250Url = app.globalData.doubanBase + "/v2/movie/top250" + "?start=0&count=3";
-        this.getMovieListData(inTheatersUrl, "inTheaters");
-        this.getMovieListData(comingSoonUrl, "comingSoon");
-        this.getMovieListData(top250Url, "top250");
+        this.getMovieListData(inTheatersUrl, "inTheaters", "正在热映");
+        this.getMovieListData(comingSoonUrl, "comingSoon", "即将上映");
+        this.getMovieListData(top250Url, "top250", "豆瓣Top250");
     },
 
-    getMovieListData: function (url, settedKey) {
+    onMoreTap:function(event){
+        var category = event.currentTarget.dataset.category;
+        wx.navigateTo({
+            url: 'more-movie/more-movie?category=' + category,
+        })
+    },
+
+    getMovieListData: function (url, settedKey, categoryTitle) {
         var that = this;
         wx: wx.request({
             url: url,
@@ -29,7 +36,7 @@ Page({
             method: 'GET',
             success: function (res) {
                 console.log(res);
-                that.processDoubanData(res.data, settedKey);
+                that.processDoubanData(res.data, settedKey, categoryTitle);
             },
             fail: function (res) {
                 console.log("failed");
@@ -37,7 +44,7 @@ Page({
         })
     },
 
-    processDoubanData: function (moviesDouban, settedKey) {
+    processDoubanData: function (moviesDouban, settedKey, categoryTitle) {
         var movies = [];
         for (var idx in moviesDouban.subjects) {
             var subject = moviesDouban.subjects[idx];
@@ -56,6 +63,7 @@ Page({
         }
         var readyData = {};
         readyData[settedKey] = {
+            categoryTitle: categoryTitle,
             movies: movies
         };
         this.setData(readyData);
